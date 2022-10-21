fedb1e2815a871cd119f57afc8eac0fa52f1cd0b
User <arinkakogyt@gmail.com>
2022-10-21 22:21:18 +0700

    создана страница с сотрудниками

---
+++ src/app/staff/staff.component.ts
@@ -0,0 +1,20 @@
+import { UsersService } from './../services/users.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff',
+  templateUrl: './staff.component.html',
+  styleUrls: ['./staff.component.css']
+})
+export class StaffComponent implements OnInit {
+
+  users: any = [];
+  constructor(public usersService: UsersService) { }
+
+  ngOnInit(): void {
+   this.usersService.getAPIUsers();
+   this.users = this.usersService.getUsers()
+  console.log(this.users);}
+  
+  
+}
