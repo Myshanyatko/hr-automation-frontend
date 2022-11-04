aac3b3274ab8986f2a66447e1136e5b7876f1fc9
myshanyatko <arinkakogyt@gmail.com>
2022-11-04 18:47:32 +0700

    верстка меню, правки в хэдэре

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,21 @@ import { AuthGuard } from './guards/auth.guard';
 import { KeyComponent } from './key/key.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'users', component: StaffComponent, canActivate: [AuthGuard]},
-    {path: 'user/:id', component: UserComponent, canActivate: [AuthGuard]},
-   {path: 'key', canActivate: [KeyGuard], component: KeyComponent},
-   {path: '', redirectTo: 'users',  pathMatch: 'full'}
+  { path: 'login', component: LoginComponent },
+  { path: 'users', component: StaffComponent, canActivate: [AuthGuard] },
+  { path: 'user/:id', component: UserComponent, canActivate: [AuthGuard] },
+  { path: 'key', canActivate: [KeyGuard], component: KeyComponent },
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'restorans', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'faq', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'products', redirectTo: 'users', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
