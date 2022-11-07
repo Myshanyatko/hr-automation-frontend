6c4ab2bca2e3b87cb601c600e62ceb2f6d6b3101
myshanyatko <arinkakogyt@gmail.com>
2022-11-07 11:24:59 +0700

    верстка new user

--- src/app/staff/new-user/new-user.component.ts
+++ src/app/staff/new-user/new-user.component.ts
@@ -3,7 +3,12 @@ import { PolymorpheusContent } from '@tinkoff/ng-polymorpheus';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsersService } from './../../services/users.service';
-import { ChangeDetectionStrategy, Component, OnInit, Inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  Inject,
+} from '@angular/core';
 
 @Component({
   selector: 'app-new-user',
@@ -13,36 +18,47 @@ import { ChangeDetectionStrategy, Component, OnInit, Inject } from '@angular/cor
 })
 export class NewUserComponent implements OnInit {
   createUserForm!: FormGroup;
-  user = {'id': 1,
-  "email": null,
-  "name": null,
-  "project": null,
-  "post": null,
-  "photo": null,
-  "information": null,
-  "admin": true};
-  constructor(@Inject(TuiDialogService) private readonly dialogService: TuiDialogService,
-  private userService: UsersService) { }
+  user = {
+    id: 1,
+    date: null,
+    email: null,
+    name: null,
+    project: null,
+    post: null,
+    photo: null,
+    information: null,
+    admin: true,
+  };
+  constructor(
+    @Inject(TuiDialogService) private readonly dialogService: TuiDialogService,
+    private userService: UsersService
+  ) {}
 
   ngOnInit(): void {
-    this.createUserForm = new FormGroup(
-      {
-        'email': new FormControl('', [Validators.required, Validators.email]),
-        'name': new FormControl('', [Validators.required]),
-        'project': new FormControl('', [Validators.required]),
-        'post': new FormControl('', [Validators.required]),
-        'admin': new FormControl(false),
-        
-      })
+    this.createUserForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      name: new FormControl('', [Validators.required]),
+      date: new FormControl(''),
+      project: new FormControl(''),
+      post: new FormControl(''),
+      information: new FormControl(''),
+      admin: new FormControl(false),
+    });
   }
-  submitSaveUser(content: PolymorpheusContent<TuiDialogContext>){
-    this.user = { ...this.user, name: this.createUserForm.value.name, email: this.createUserForm.value.email, project: this.createUserForm.value.project, post: this.createUserForm.value.post, admin: this.createUserForm.value.admin }
+  saveUser(content: PolymorpheusContent<TuiDialogContext>) {
+    this.user = {
+      ...this.user,
+      name: this.createUserForm.value.name,
+      date: this.createUserForm.value.date,
+      email: this.createUserForm.value.email,
+      project: this.createUserForm.value.project,
+      post: this.createUserForm.value.post,
+      admin: this.createUserForm.value.admin,
+      information: this.createUserForm.value.admin,
+    };
     this.userService.postAPIUser(this.user);
     console.log('new user is created');
     this.dialogService.open(content).subscribe();
-    this.ngOnInit()
+    this.ngOnInit();
   }
-
-  
-
 }
