2298a9aa1a20c45d37d8181054a1b578852fd4d2
myshanyatko <arinkakogyt@gmail.com>
2022-11-14 23:31:18 +0700

    список вопросов переделан в аккордионы, добавлен инпут под поиск и пару кнопок

--- src/app/faq/faq-list/faq-list.component.ts
+++ src/app/faq/faq-list/faq-list.component.ts
@@ -11,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqListComponent implements OnInit {
   categories$ = this.store$.select(selectCategories);
+  search = '';
   constructor(private store$: Store<AppState>) {}
 
   ngOnInit(): void {
